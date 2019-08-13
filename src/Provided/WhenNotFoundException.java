8e2c912f9f7fa4e564a645b8549ef7cf73c948d1
Nizar Elias <nizar-elias@hotmail.com>
2019-08-13 20:34:29 +0300

    reset

--- src/Provided/WhenNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package Provided;
-
-/**
- * You should throw this exception when there isn't any method
- * annotated by the When annotation (and has the same sentence),
- * which you can invoke.
- */
-public class WhenNotFoundException extends WordNotFoundException {
-}
