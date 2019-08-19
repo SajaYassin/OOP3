7fdfaafbecf38d97496770f97cdde9ab2882fdf5
SajaYassin <saja.yassin@gmail.com>
2019-08-19 20:13:19 +0300

    add comapre story to value

--- src/Solution/StoryTesterImpl.java
+++ src/Solution/StoryTesterImpl.java
@@ -1,23 +1,46 @@
 package Solution;
-
 import java.util.Arrays;
 
 public class StoryTesterImpl implements StoryTester {
 
+     private boolean checkEquality(String[][] s1, String[][] s2) {
+        if (s1 == s2)
+            return true;
+
+        if (s1 == null || s2 == null)
+            return false;
+
+        int n = s1.length;
+        if (n != s2.length)
+            return false;
+
+        for (int i = 0; i < n; i++) {
+            if (s1[i].length != s2[i].length)
+                return false;
+
+            for (int j = 0; j < s1[i].length; j++) {
+                if (!s1[i][j].equals(s2[i][j]))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+// i didn't test this part
     private boolean compareSroryAndAnottaion(String value, String story){
-        String[] subSnt1Value = value.split("and ");
-        String[] subSnt2Story = (value.split("or "))[0].split("and ");
-        return false;
+        String[][] valueLines = removeLastWord(value.split("and "));
+        String[][] storyLines = removeLastWord(value.split("or ")[0].split("and "));
+        return checkEquality(valueLines,storyLines);
     }
 
     private String[][] removeLastWord(String[] lines){
         String[][] result;
         result = new String[lines.length][];
         for (int i =0 ; i < lines.length; i++){
-            //result[i] =
+            String[] line = lines[i].split(" ");
+            result[i] = Arrays.copyOf(line,line.length -1);
         }
-
-        return new String[0][];
+        return result;
     }
 
     @Override
